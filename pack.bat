c4de0d2a72c6e35ea95f12058ade9b4ec84cd55a
Jared Flores <jared_flores@cs.ucsb.edu>
2020-10-04 19:31:15 -0700

    updated pack

--- pack.bat
+++ pack.bat
@@ -1,4 +1,7 @@
 @echo off
 zip.vbs %~dp0pack\Anniversary_Edition %~dp0Anniversary_Edition.zip
 copy %~dp0Anniversary_Edition.zip %appdata%\.minecraft\resourcepacks
+git add .
+git commit -m "updated pack"
+git push
 CertUtil -hashfile %~dp0Anniversary_Edition.zip SHA1

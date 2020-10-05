30cde7283dd6b30699c5d0fde013221e6ae4d7bc
Jared Flores <jared_flores@cs.ucsb.edu>
2020-10-04 18:34:17 -0700

    yeet

--- pack.bat
+++ pack.bat
@@ -1,3 +1,4 @@
 @echo off
 zip.vbs %~dp0pack\Anniversary_Edition %~dp0Anniversary_Edition.zip
 copy %~dp0Anniversary_Edition.zip %appdata%\.minecraft\resourcepacks
+CertUtil -hashfile %~dp0Anniversary_Edition.zip SHA1
--- src/io/mewsub/anniversary/commands/AnniversaryCommands.java
+++ src/io/mewsub/anniversary/commands/AnniversaryCommands.java
@@ -2,6 +2,8 @@ package io.mewsub.anniversary.commands;
 
 import io.mewsub.anniversary.Anniversary;
 
+import java.util.Random;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -15,13 +17,17 @@ public class AnniversaryCommands implements CommandExecutor {
 		if( args.length > 0 ) {
 			String cmd = args[ 0 ];
 			if( cmd.equalsIgnoreCase( "pack" ) ) {
-				if( args.length != 22 ) return true;
+				if( args.length != 22 && args.length != 2 && args.length != 1 ) return true;
 				byte[] hash = new byte[ 20 ];
-				for( int i = 2; i < 22; ++i ) {
-					hash[ i ] = ( byte ) ( ( Integer.parseInt( args[ i ], 16 ) & 0xFF ) - 0x100 );
+				if( args.length == 22 ) {
+					for( int i = 2; i < 22; ++i ) {
+						hash[ i - 2 ] = ( byte ) ( ( Integer.parseInt( args[ i ], 16 ) & 0xFF ) - 0x100 );
+					}	
 				}
 				for( Player p : Anniversary.server.getOnlinePlayers() ) {
-					p.setResourcePack( args[ 1 ], hash );
+					if( args.length == 22 ) p.setResourcePack( args[ 1 ], hash );
+					else if( args.length == 2 ) p.setResourcePack( args[ 1 ] );
+					else p.setResourcePack( "https://github.com/jay-red/Anniversary/raw/main/Anniversary_Edition.zip?rev=" + ( new Random() ).nextInt() );
 				}
 			}
 		}
